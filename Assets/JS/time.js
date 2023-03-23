95131ef178bb107b872baa11836b8bdd010f4a53
Mike <124840291+mikewashereGH@users.noreply.github.com>
2023-03-23 12:06:13 -0400

    y

---
+++ Assets/JS/time.js
@@ -0,0 +1,10 @@
+var today = new Date()
+var curHr = today.getHours()
+
+if (curHr < 12) {
+    console.log('Good Morning')
+} else if (curHr < 18) {
+    console.log('Good Afternoon')
+} else {
+    console.log('Good Evening')
+}
