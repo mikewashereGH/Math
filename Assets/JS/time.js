d4e967e0e961a154c42c72d7dfafacf0fbb7b519
Mike <124840291+mikewashereGH@users.noreply.github.com>
2023-03-23 12:16:17 -0400

    u

--- Assets/JS/time.js
+++ Assets/JS/time.js
@@ -1,10 +1,16 @@
 var today = new Date()
 var curHr = today.getHours()
+let word;
 
 if (curHr < 12) {
-    console.log('Good Morning')
+    word = 'Good Morning';
 } else if (curHr < 18) {
-    console.log('Good Afternoon')
+    word = 'Good Afternoon';
 } else {
-    console.log('Good Evening')
+    word = 'Good Evening';
 }
+
+function changeColor(newColor) {
+    const elem = document.getElementById("good_time");
+    elem.innerHTML = word;
+  }
